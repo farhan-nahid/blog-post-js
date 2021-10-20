1a724bbb22be43ee59045428e6c2ffec5cf2e009
gesora96 <nahidahmed9933@gmail.com>
2021-10-20 21:55:32 +0600

    formated

--- index.js
+++ index.js
@@ -52,31 +52,30 @@
   </style>
   `;
   const skeleton = `
-  <article class="post">
-<div class="post__meta">
-<div class="post__tag--container">
-<span class="skeleton post__tag"></span>
-<span class="skeleton post__tag"></span>
-</div>
-<p class="skeleton post__date"></p>
-</div>
-<h3 class="skeleton post__header">
-
-</h3>
-<div class="post__author">
-<div class="skeleton post__author--avatar" width="55" ></div>
-<div>
-<p class="skeleton post__author--name"></p>
-<p class="skeleton post__author--role"><small></small></p>
-</div>
-</div>
-<div class="post__body">
-<p class="skeleton"></p>
-<p class="skeleton"></p>
-<p class="skeleton"></p>
-<p class="skeleton"></p>
-</div>
-<a href="#" class="skeleton btn"></a></article>
+        <article class="post">
+          <div class="post__meta">
+            <div class="post__tag--container">
+              <span class="skeleton post__tag"></span>
+              <span class="skeleton post__tag"></span>
+            </div>
+            <p class="skeleton post__date"></p>
+          </div>
+          <h3 class="skeleton post__header"></h3>
+          <div class="post__author">
+            <div class="skeleton post__author--avatar" width="55" ></div>
+            <div>
+              <p class="skeleton post__author--name"></p>
+              <p class="skeleton post__author--role"><small></small></p>
+            </div>
+          </div>
+          <div class="post__body">
+            <p class="skeleton"></p>
+            <p class="skeleton"></p>
+            <p class="skeleton"></p>
+            <p class="skeleton"></p>
+          </div>
+          <a href="#" class="skeleton btn"></a>
+       </article>
   `;
   document.querySelector(".post-container").innerHTML = [
     skeleton.repeat(3),
